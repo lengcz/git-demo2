7bbf1c32a860827dc428876a02dbe357a273aec2
lengcz <lcz0710@163.com>
2022-04-19 01:06:49 +0800

    修改了内容

--- src/main/java/it2/TestGit.java
+++ src/main/java/it2/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("version 1");
         System.out.println("version 2");
+        System.out.println("version 3 by dev");
 
     }
 }
